47ddbdce118f1ac421a904024cb42dccd7f6f1b9
isuraem <erandaim@gmail.com>
2024-06-11 23:10:55 +0530

    changes_in_Card

--- src/app/components/question-card/questionCard.tsx
+++ src/app/components/question-card/questionCard.tsx
@@ -17,7 +17,7 @@ interface CardProps {
 const QuestionCard: React.FC<CardProps> = ({ questionTitle, tags, user }) => {
 
     return (
-        <div className="w-full max-w-full p-4 bg-white border border-gray-200 rounded-lg shadow-md">
+        <div className="w-full max-w-full p-4 bg-white border border-gray-200 rounded-lg shadow-md mb-3">
             <div className="mb-2 text-lg font-medium text-blue-950">
                 <Link href="./questions/answer-question">
                     {questionTitle}
@@ -27,8 +27,6 @@ const QuestionCard: React.FC<CardProps> = ({ questionTitle, tags, user }) => {
                 {tags.length > 0 && tags.map((tag: any, index: number) => (
                    <span className="px-2 py-1 mr-2 text-xs font-semibold text-blue-800 bg-blue-100 rounded">{tag.name}</span>
                 ))}
-                <span className="px-2 py-1 mr-2 text-xs font-semibold text-indigo-500 bg-blue-100 rounded">Python</span>
-                <span className="px-2 py-1 text-xs font-semibold text-indigo-500 bg-blue-100 rounded">Linked list</span>
             </div>
             <div className="flex justify-between items-center text-gray-500">
                 <div className="flex flex-row">
@@ -43,7 +41,7 @@ const QuestionCard: React.FC<CardProps> = ({ questionTitle, tags, user }) => {
                 </div>
                 <div className="flex items-center">
                     <div className="w-8 h-8 rounded-full bg-gray-200 mr-2"></div>
-                    <span className="text-sm font-medium text-blue-950">Damon Salvatore</span>
+                    <span className="text-sm font-medium text-blue-950">{user.name}</span>
                 </div>
             </div>
         </div>
